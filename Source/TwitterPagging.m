f6ae62fffda09621969e5dbe9b87dd8c2bd8f55d
Jack <xhzengAIB@gmail.com>
2014-06-20 22:26:25 +0000

    初始化工程

---
+++ Source/TwitterPagging.m
@@ -0,0 +1,21 @@
+//
+//  TwitterPagging.m
+//  TwitterPagging
+//
+//  Created by 曾 宪华 on 14-6-20.
+//  Copyright (c) 2014年 曾宪华 QQ群: (142557668) QQ:543413507  Gmail:xhzengAIB@gmail.com. All rights reserved.
+//
+
+#import "TwitterPagging.h"
+
+@implementation TwitterPagging
+
+- (id)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+@end
