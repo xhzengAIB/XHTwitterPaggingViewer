84e2abf3385ea2ee949ff3032a527a9a899be09d
Jack <xhzengAIB@gmail.com>
2014-06-21 01:04:31 +0000

    删除多余文件

--- XHTwitterPaggingViewerExample/XHTwitterPaggingViewerExampleTests/XHTwitterPaggingViewerExampleTests.m
+++ XHTwitterPaggingViewerExample/XHTwitterPaggingViewerExampleTests/XHTwitterPaggingViewerExampleTests.m
@@ -1,18 +1,18 @@
 //
-//  TwitterPaggingTests.m
-//  TwitterPaggingTests
+//  XHTwitterPaggingViewerExampleTests.m
+//  XHTwitterPaggingViewerExampleTests
 //
-//  Created by 曾 宪华 on 14-6-20.
+//  Created by 曾 宪华 on 14-6-21.
 //  Copyright (c) 2014年 曾宪华 QQ群: (142557668) QQ:543413507  Gmail:xhzengAIB@gmail.com. All rights reserved.
 //
 
 #import <XCTest/XCTest.h>
 
-@interface TwitterPaggingTests : XCTestCase
+@interface XHTwitterPaggingViewerExampleTests : XCTestCase
 
 @end
 
-@implementation TwitterPaggingTests
+@implementation XHTwitterPaggingViewerExampleTests
 
 - (void)setUp
 {
